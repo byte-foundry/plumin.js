9f4832eb10c12132880a7a4d17132ebaa5d10aa7
Louis-Rémi Babé <lrbabe@gmail.com>
2016-02-18 11:41:43 +0100

    commit missing files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+	entry: [ './src/plumin.js' ],
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'plumin.js',
+		library: 'plumin',
+        libraryTarget: 'umd',
+	},
+	resolve: {
+		alias: {
+			'paper': 'paper/dist/paper-core.js',
+		}
+	},
+	externals: [{
+		'./node/window': true,
+		'./node/extend': true,
+	}],
+};
