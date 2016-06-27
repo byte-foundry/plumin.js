71318e467028c2a4862728c18c2b1c270c4c002b
Martin Bolot <martin@martinbolot.com>
2016-06-27 18:25:40 +0200

    modified font download

--- src/Font.js
+++ src/Font.js
@@ -340,26 +340,28 @@ if ( typeof window === 'object' && window.document ) {
 	};
 
 	Font.prototype.download = function( arrayBuffer, merged, name, user ) {
-		if ( merged ) {
-			// TODO: replace that with client-side font merging
-			fetch('https://merge.prototypo.io/' +
-				name.family + '/' +
-				name.style + '/' + user, {
-					method: 'POST',
-					headers: { 'Content-Type': 'application/otf' },
-					body: arrayBuffer
-			})
-			.then(function( response ) {
-				return response.arrayBuffer();
-			})
-			.then(function( bufferToDownload ) {
+		// TODO: replace that with client-side font merging
+		fetch('http://localhost:3000/' +
+			name.family + '/' +
+			name.style + '/' +
+			user +
+			(name.template ? '/' + name.template : '') +
+			(merged ? '/overlap' : ''), {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/otf' },
+				body: arrayBuffer
+		})
+		.then(function( response ) {
+			return response.arrayBuffer();
+		})
+		.then(function( bufferToDownload ) {
+			if( merged ) {
 				triggerDownload( this, bufferToDownload );
-			}.bind(this));
-
-		} else {
-			triggerDownload(
-				this, arrayBuffer, name && ( name.family + ' ' + name.style ) );
-		}
+			} else {
+				triggerDownload(
+					this, arrayBuffer, name && ( name.family + ' ' + name.style ) );
+			}
+		}.bind(this));
 
 		return this;
 	};
