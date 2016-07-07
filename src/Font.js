a6c889d9c7f261fac5441dfdee298b8ac0b2a91c
FranzPoize <francois.poizat@gmail.com>
2016-07-07 15:55:15 +0200

    reaching for merge.prototypo.io

--- src/Font.js
+++ src/Font.js
@@ -347,7 +347,7 @@ if ( typeof window === 'object' && window.document ) {
 				name && ( name.family + ' ' + name.style ) );
 		}
 		// TODO: replace that with client-side font merging
-		fetch('http://localhost:3000/' +
+		fetch('http://merge.prototypo.io/' +
 			name.family + '/' +
 			name.style + '/' +
 			user +
